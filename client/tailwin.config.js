e063d0ca0c76d906d62247220df505048fd47eb9
Guy Baseke <guy.baseke@eunasolutions.com>
2025-06-20 15:30:15 -0400

    End of Section 8

---
+++ client/tailwin.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,ts}', // Adjust paths based on your project structure
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1D4ED8',
+                secondary: '#9333EA',
+            },
+        },
+    },
+    plugins: [],
+};
